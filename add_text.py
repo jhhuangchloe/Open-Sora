4c71fce9916b8a3f094cfd8923494de920328f6f
Jiahe (Chloe) Huang <94778483+jhhuangchloe@users.noreply.github.com>
2024-07-08 12:15:43 +0800

    print aspect ratios

--- add_text.py
+++ add_text.py
@@ -5,7 +5,7 @@ file_path = 'NS_video_vinfo.csv'  # 替换为你的 CSV 文件路径
 df = pd.read_csv(file_path)
 
 # 添加新列 'text' 并将其值设置为 'caption'
-df['text'] = ''
+df['text'] = 'caption'
 
 # 保存修改后的 CSV 文件
 output_file_path = 'NS_video_new.csv'  # 设置输出文件路径
