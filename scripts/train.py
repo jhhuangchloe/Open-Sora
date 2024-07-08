ecdace3d7386103dab30b0b0d958735db8c0093c
Jiahe (Chloe) Huang <94778483+jhhuangchloe@users.noreply.github.com>
2024-07-08 13:27:52 +0800

    print num_frames

--- scripts/train.py
+++ scripts/train.py
@@ -110,8 +110,8 @@ def main():
     )
     num_steps_per_epoch = len(dataloader)
     # see the dimension of data in dataloader
-    print(next(iter(dataloader))['video'].shape)
-    print(next(iter(dataloader))['num_frames'])
+    print(f"shape: {next(iter(dataloader))['video'].shape()}")
+    print(f"number of frames: {next(iter(dataloader))['num_frames']}")
 
     # ======================================================
     # 3. build model
